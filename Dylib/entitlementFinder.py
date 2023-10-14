2eb21e4fb8bd7c9781a3f3b7eb4c2f66e959ff9d
0xv1n <11021725+0xv1n@users.noreply.github.com>
2023-10-14 08:58:23 -0400

    Adds man page grepping
    Automates scanning of man pages of matched binaries.

--- Dylib/entitlementFinder.py
+++ Dylib/entitlementFinder.py
@@ -1,19 +1,24 @@
 import subprocess
 import os
 import argparse
+import re
 
-# Set a default path if you want, else specify as arg
-# Usage: entitlementFinder.py /usr/bin
-directory_to_scan = '/path/to/your/directory'
-results = {}
+def get_binary_name(file_path):
+    return os.path.basename(file_path)
 
 def check_codesign(file_path):
     try:
         output = subprocess.check_output(['codesign', '-d', '--entitlements', '-', file_path], stderr=subprocess.STDOUT, universal_newlines=True)
-        if 'com.apple.security.cs.disable-library-validation' in output:
-            results[file_path] = output
+        return output
     except subprocess.CalledProcessError as e:
-        pass
+        return ""
+
+def search_man_pages(binary_name):
+    try:
+        man_output = subprocess.check_output(['man', binary_name], stderr=subprocess.STDOUT, universal_newlines=True)
+        return man_output
+    except subprocess.CalledProcessError as e:
+        return ""
 
 def main():
     parser = argparse.ArgumentParser(description='')
@@ -21,15 +26,30 @@ def main():
     args = parser.parse_args()
 
     directory_to_scan = args.directory
+    matched_binaries = []
 
     for root, _, files in os.walk(directory_to_scan):
         for file in files:
             file_path = os.path.join(root, file)
-            check_codesign(file_path)
+            binary_name = get_binary_name(file_path)
+            codesign_output = check_codesign(file_path)
+            if 'com.apple.security.cs.disable-library-validation' in codesign_output:
+                man_output = search_man_pages(binary_name)
+                matched_lines = [line for line in man_output.split('\n') if re.search(r'\b(library|libraries)\b', line, re.I)]
+                if matched_lines:
+                    matched_binaries.append((binary_name, file_path, codesign_output, matched_lines))
 
-    for file_path, output in results.items():
-        print(f"File: {file_path}")
-        print(output)
+    if matched_binaries:
+        print("Binaries with matched content:")
+        for binary_name, file_path, codesign_output, matched_lines in matched_binaries:
+            print(f"Binary Name: {binary_name}")
+            print(f"File Path: {file_path}")
+            print("Output from codesign:")
+            print(codesign_output)
+            print("Matched Lines from man page:")
+            for line in matched_lines:
+                print(line)
+            print()
 
 if __name__ == "__main__":
     main()
